59e610092873c3b17b9f1e848dee9ecabe0aa988
neel-1905 <neelnarwadkar@gmail.com>
2026-06-01 09:52:15 +0530

    expo plugin

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,17 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "@/db";
+import * as schema from "@/db/schema";
+import { expo } from "@better-auth/expo";
+
 export const auth = betterAuth({
+  plugins: [expo()],
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema,
   }),
+
+  trustedOrigins: ["*"],
 
   emailAndPassword: {
     enabled: true,
